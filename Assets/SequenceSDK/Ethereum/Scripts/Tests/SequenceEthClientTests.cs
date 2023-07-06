b3fbeb51f86810219fa1c7719e1db338fa8b592b
Quinn Purdy <qp@horizon.io>
2023-07-06 15:54:45 -0400

    Use async Tasks and await for SequenceEthClientTests instead of coroutines.

--- Assets/SequenceSDK/Ethereum/Scripts/Tests/SequenceEthClientTests.cs
+++ Assets/SequenceSDK/Ethereum/Scripts/Tests/SequenceEthClientTests.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Sequence.Mocks;
 using Sequence.Provider;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -9,107 +10,46 @@ public class RPCTests
 {
     string clientUrl = "http://localhost:8545/";
     float polygonBlockTimeInSeconds = 2f;
-    
-    [UnityTest]
-    public IEnumerator TestChainID()
-    {
-        string expected = "0x7a69";
-        var client = new SequenceEthClient(clientUrl);
 
-        Task<string> chainIdTask = client.ChainID();
-        while (!chainIdTask.IsCompleted)
-        {
-            yield return null;
-        }
-
-        if (chainIdTask.Exception != null)
-        {
-            Assert.Fail("Unexpected exception: " + chainIdTask.Exception.Message);
-            yield break;
-        }
-        string chainId = chainIdTask.Result;
-        Assert.AreEqual(expected, chainId);
-    }
-
-    [UnityTest]
-    public IEnumerator TestNetworkId()
-    {
-        string expected = "31337";
-        var client = new SequenceEthClient(clientUrl);
-
-        Task<string> networkIdTask = client.NetworkId();
-        while (!networkIdTask.IsCompleted)
-        {
-            yield return null;
-        }
-
-        if (networkIdTask.Exception != null)
-        {
-            Assert.Fail("Unexpected exception: " + networkIdTask.Exception.Message);
-            yield break;
-        }
-        string networkId = networkIdTask.Result;
-        Assert.AreEqual(expected, networkId);
-    }
-
-    [UnityTest]
-    public IEnumerator TestBlockNumber()
-    {
+    [Test]
+    public async Task TestChainId() {
+        try {
+            string expected = "0x7a69";
             var client = new SequenceEthClient(clientUrl);
+            string chainId = await client.ChainID();
+            Assert.AreEqual(expected, chainId);
+        }
+        catch (System.Exception ex) {
+            Assert.Fail("Expected no exception, but got: " + ex.Message);
+        }
+    }
 
-            Task<string> blockNumberTask = client.BlockNumber();
-            while (!blockNumberTask.IsCompleted)
-            {
-                yield return null;
-            }
+    [Test]
+    public async Task TestNetworkId() {
+        try {
+            string expected = "31337";
+            var client = new SequenceEthClient(clientUrl);
+            string networkId = await client.NetworkId();
+            Assert.AreEqual(expected, networkId);
+        }
+        catch (System.Exception ex) {
+            Assert.Fail("Expected no exception, but got: " + ex.Message);
+        }
+    }
 
-            if (blockNumberTask.Exception != null)
-            {
-                Assert.Fail("Unexpected exception: " + blockNumberTask.Exception.Message);
-                yield break;
-            }
-            var blockNumber = blockNumberTask.Result;
-
-            yield return new WaitForSecondsRealtime(polygonBlockTimeInSeconds * 3); // Wait for more than the block time just in case
-
-            Task<string> blockNumberTask2 = client.BlockNumber();
-            while (!blockNumberTask2.IsCompleted)
-            {
-                yield return null;
-            }
-
-            if (blockNumberTask2.Exception != null)
-            {
-                Assert.Fail("Unexpected exception: " + blockNumberTask2.Exception.Message);
-                yield break;
-            }
-            var blockNumber2 = blockNumberTask2.Result;
-
+    [Test]
+    public async Task TestBlockNumber() {
+        try {
+            var client = new SequenceEthClient(clientUrl);
+            var blockNumber = await client.BlockNumber();
+            await Task.Delay((int)(polygonBlockTimeInSeconds * 1000 * 3)); // Wait for more than the block time just in case
+            var blockNumber2 = await client.BlockNumber();
             Assert.Less(blockNumber, blockNumber2);
+        }
+        catch (System.Exception ex) {
+            Assert.Fail("Expected no exception, but got: " + ex.Message);
+        }
     }
 
-    [UnityTest]
-    public IEnumerator TestERC20MintAndTransfer()
-    {
-        yield return null;
-    }
-
-    [UnityTest]
-    public IEnumerator TestBlockByNumber()
-    {
-        yield return null;
-    }
-
-    [UnityTest]
-    public IEnumerator ExampleBatchCall()
-    {
-        yield return null;
-    }
-
-    [UnityTest]
-    public IEnumerator TestETHRPC()
-    {
-        yield return null;
-    }
 
 }
