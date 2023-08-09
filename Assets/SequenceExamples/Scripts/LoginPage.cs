f74e1a49abc8e146830cc01f11866b02334ecf82
Quinn Purdy <qp@horizon.io>
2023-08-09 16:14:12 -0400

    Added a UIPage class that contains the base logic for a UI screen/page (e.g. open/close methods) and made ConnectPage and LoginPage inherrit from it

--- Assets/SequenceExamples/Scripts/LoginPage.cs
+++ Assets/SequenceExamples/Scripts/LoginPage.cs
@@ -3,44 +3,8 @@ using UnityEngine;
 
 namespace Sequence.Demo
 {
-    [RequireComponent(typeof(RectTransform))]
-    public class LoginPage : MonoBehaviour
+    public class LoginPage : UIPage
     {
-        private RectTransform _transform;
-        [SerializeField] private float _openAnimationDurationInSeconds;
-        [SerializeField] private float _closeAnimationDurationInSeconds;
-        private GameObject _gameObject;
-        private Scale _scale;
-
-        private void Awake()
-        {
-            _transform = GetComponent<RectTransform>();
-            _gameObject = gameObject;
-            _scale = _gameObject.AddComponent<Scale>();
-            _scale.Initialize(_transform);
-            _transform.localScale = new Vector3(0, 0, 0);
-        }
-
-        private void Start()
-        {
-            _gameObject.SetActive(false);
-        }
-
-        public void Open()
-        {
-            _gameObject.SetActive(true);
-            _scale.ScaleOverTime(1, _openAnimationDurationInSeconds);
-        }
-
-        public void Close()
-        {
-            _scale.ScaleOverTime(0, _closeAnimationDurationInSeconds);
-            Invoke(nameof(Deactivate), _closeAnimationDurationInSeconds);
-        }
-
-        private void Deactivate()
-        {
-            _gameObject.SetActive(false);
-        }
+        
     }
 }
