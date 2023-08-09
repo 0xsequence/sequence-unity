e0c802ef672a5a69ef617d8e274d95bc350dab88
Quinn Purdy <qp@horizon.io>
2023-08-09 16:09:36 -0400

    Added logic to switch UI pages, supports 'back' (needs to be mapped to the back key on various platforms, but for now, we have a UI button). Added a very minimal login screen for testing this out.

--- Assets/SequenceExamples/Scripts/LoginPage.cs
+++ Assets/SequenceExamples/Scripts/LoginPage.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Sequence.Demo.Tweening;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Sequence.Demo
 {
     [RequireComponent(typeof(RectTransform))]
-    public class UIConnect : MonoBehaviour
+    public class LoginPage : MonoBehaviour
     {
         private RectTransform _transform;
         [SerializeField] private float _openAnimationDurationInSeconds;
--- Assets/SequenceExamples/Scripts/SequenceUI.cs
+++ Assets/SequenceExamples/Scripts/SequenceUI.cs
@@ -7,16 +7,91 @@ namespace Sequence.Demo
 {
     public class SequenceUI : MonoBehaviour
     {
-        private UIConnect connect;
+        private ConnectPage _connectPage;
+        private LoginPage _loginPage;
         
+        public enum UIState
+        {
+            Connect,
+            Login,
+            TwoFactorAuthentication,
+            Wallet,
+            Default,
+        }
+
+        public UIState State { get; private set; } = UIState.Default;
+
+        private Stack<UIState> _stateStack = new Stack<UIState>();
+
         private void Awake()
         {
-            connect = FindObjectOfType<UIConnect>();
+            _connectPage = FindObjectOfType<ConnectPage>();
+            _loginPage = FindObjectOfType<LoginPage>();
         }
 
         private void Start()
         {
-            connect.Open();
+            SetUIState(UIState.Connect);
+        }
+
+        public void SetUIState(UIState desiredState)
+        {
+            ClosePage();
+            State = desiredState;
+            _stateStack.Push(desiredState);
+            OpenPage();
+        }
+
+        private void ClosePage()
+        {
+            switch (State)
+            {
+                case UIState.Connect:
+                    _connectPage.Close();
+                    break;
+                case UIState.Login:
+                    _loginPage.Close();
+                    break;
+                case UIState.TwoFactorAuthentication:
+                    break;
+                case UIState.Wallet:
+                    break;
+            }
+        }
+
+        private void OpenPage()
+        {
+            switch (State)
+            {
+                case UIState.Connect:
+                    _connectPage.Open();
+                    break;
+                case UIState.Login:
+                    _loginPage.Open();
+                    break;
+                case UIState.TwoFactorAuthentication:
+                    break;
+                case UIState.Wallet:
+                    break;
+            }
+        }
+
+        public void Back()
+        {
+            if (_stateStack.Count <= 1)
+            {
+                return;
+            }
+
+            ClosePage();
+            _stateStack.Pop();
+            State = _stateStack.Peek();
+            OpenPage();
+        }
+
+        public void OpenLoginPage()
+        {
+            SetUIState(UIState.Login);
         }
     }
 }
