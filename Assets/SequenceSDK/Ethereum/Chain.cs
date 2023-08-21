17e96819797ffe4db8382950d5a4b3ad7793d760
Quinn Purdy <qp@horizon.io>
2023-08-21 10:07:36 -0400

    Added support for newly supported indexer chains

--- Assets/SequenceSDK/Ethereum/Chain.cs
+++ Assets/SequenceSDK/Ethereum/Chain.cs
@@ -7,17 +7,22 @@ namespace Sequence
         // Mainnets
         public static readonly BigInteger Ethereum = 1;
         public static readonly BigInteger Polygon = 137;
+        public static readonly BigInteger PolygonZkEvm = 1101;
         public static readonly BigInteger BNBSmartChain = 56;
         public static readonly BigInteger ArbitrumOne = 42161;
         public static readonly BigInteger ArbitrumNova = 42170;
         public static readonly BigInteger Optimism = 10;
         public static readonly BigInteger Avalanche = 43114;
         public static readonly BigInteger Gnosis = 100;
+        public static readonly BigInteger Base = 8453;
 
         // Testnets
         public static readonly BigInteger TestnetGoerli = 5;
+        public static readonly BigInteger TestnetSepolia = 11155111;
         public static readonly BigInteger TestnetPolygonMumbai = 80001;
+        public static readonly BigInteger TestnetArbitrumGoerli = 421613;
         public static readonly BigInteger TestnetBNBSmartChain = 97;
         public static readonly BigInteger TestnetAvalancheFuji = 43113;
+        public static readonly BigInteger TestnetBaseGoerli = 84531;
     }
 }
