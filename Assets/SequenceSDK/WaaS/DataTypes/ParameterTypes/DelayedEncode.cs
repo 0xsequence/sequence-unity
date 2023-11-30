5b74f2d9b3774c7a59d5f5231496922e40f1f2e7
Quinn Purdy <qp@horizon.io>
2023-11-30 10:10:51 -0500

    Fixed canonical ordering for parameter types so that transactions go through.

--- Assets/SequenceSDK/WaaS/DataTypes/ParameterTypes/DelayedEncode.cs
+++ Assets/SequenceSDK/WaaS/DataTypes/ParameterTypes/DelayedEncode.cs
@@ -5,8 +5,8 @@ namespace SequenceSDK.WaaS
     {
         public const string TypeIdentifier = "delayedEncode";
         public DelayedEncodeData data { get; private set; }
-        public string type { get; private set; } = TypeIdentifier;
         public string to { get; private set; }
+        public string type { get; private set; } = TypeIdentifier;
         public string value { get; private set; }
         
         public DelayedEncode(string to, string value, DelayedEncodeData data)
