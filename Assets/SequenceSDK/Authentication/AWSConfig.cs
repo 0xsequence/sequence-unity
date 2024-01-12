3d38d851235e430aa3b8e400408dfe9f334e6046
Quinn Purdy <qp@horizon.io>
2024-01-12 16:01:43 -0500

    Throw errors when we try to access a config value that is null or empty. The only exception being in SetUrlScheme, here we only log a warning, we don't want to stop a build because of a missing Url Scheme as they may not be using social login - in this case, they will receive an exception as usual if they then try to do social login.

--- Assets/SequenceSDK/Authentication/AWSConfig.cs
+++ Assets/SequenceSDK/Authentication/AWSConfig.cs
@@ -1,3 +1,6 @@
+using System;
+using Sequence.Config;
+
 namespace Sequence.Authentication
 {
     public class AWSConfig
@@ -9,9 +12,25 @@ namespace Sequence.Authentication
 
         public AWSConfig(string region, string identityPoolId, string kmsEncryptionKeyId, string cognitoClientId)
         {
+            if (string.IsNullOrWhiteSpace(region))
+            {
+                throw SequenceConfig.MissingConfigError("Region");
+            }
             Region = region;
+            if (string.IsNullOrWhiteSpace(identityPoolId))
+            {
+                throw SequenceConfig.MissingConfigError("Identity Pool Id");
+            }
             IdentityPoolId = identityPoolId;
+            if (string.IsNullOrWhiteSpace(kmsEncryptionKeyId))
+            {
+                throw SequenceConfig.MissingConfigError("KMS Encryption Key Id");
+            }
             KMSEncryptionKeyId = kmsEncryptionKeyId;
+            if (string.IsNullOrWhiteSpace(cognitoClientId))
+            {
+                throw SequenceConfig.MissingConfigError("Cognito Client Id");
+            }
             CognitoClientId = cognitoClientId;
         }
     }
