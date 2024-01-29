4119131650e9dcc22ea9f85edb0534819b4d6067
Quinn Purdy <qp@horizon.io>
2024-01-29 14:23:58 -0500

    For security purposes, include session wallet address as a nonce when signing in via OIDC. Similarly, include session wallet address as metadata when responding to auth challenge for email sign in

--- Assets/SequenceSDK/Authentication/OpenIdAuthenticator.cs
+++ Assets/SequenceSDK/Authentication/OpenIdAuthenticator.cs
@@ -24,9 +24,10 @@ namespace Sequence.Authentication
         private static readonly string RedirectUrl = "https://dev2-api.sequence.app/oauth/callback";
         
         private string _stateToken = Guid.NewGuid().ToString();
-        private readonly string _nonce = Guid.NewGuid().ToString();
 
-        public OpenIdAuthenticator()
+        private string _sessionAddress;
+
+        public OpenIdAuthenticator(string sessionAddress)
         {
             SequenceConfig config = SequenceConfig.GetConfig();
 
@@ -35,6 +36,8 @@ namespace Sequence.Authentication
             DiscordClientId = config.DiscordClientId;
             FacebookClientId = config.FacebookClientId;
             AppleClientId = config.AppleClientId;
+            
+            _sessionAddress = sessionAddress;
         }
 
         public void GoogleSignIn()
@@ -149,7 +152,7 @@ namespace Sequence.Authentication
             }
             
             string url =
-                $"{baseUrl}?response_type=id_token&client_id={clientId}&redirect_uri={RedirectUrl}&scope=openid+profile+email&state={_urlScheme + "---" + _stateToken + method}&nonce={_nonce}/";
+                $"{baseUrl}?response_type=id_token&client_id={clientId}&redirect_uri={RedirectUrl}&scope=openid+profile+email&state={_urlScheme + "---" + _stateToken + method}&nonce={_sessionAddress}/";
             if (PlayerPrefs.HasKey(LoginEmail))
             {
                 url = url.RemoveTrailingSlash() + $"&login_hint={PlayerPrefs.GetString(LoginEmail)}".AppendTrailingSlashIfNeeded();
