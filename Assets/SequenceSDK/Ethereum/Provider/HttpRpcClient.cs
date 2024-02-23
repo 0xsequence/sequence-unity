bbde3d2dc5496feb4d83a65b70ba59767faa2405
BellringerQuinn <49766546+BellringerQuinn@users.noreply.github.com>
2024-02-23 12:28:09 -0500

    Fix HttpRpcClient for SequenceEthClient such that it includes the API key. Fix FailedTransactionReturn constructor such that it handles the JObject properly and our unhappy tests pass. Upgrade package version (#92)

--- Assets/SequenceSDK/Ethereum/Provider/HttpRpcClient.cs
+++ Assets/SequenceSDK/Ethereum/Provider/HttpRpcClient.cs
@@ -1,5 +1,8 @@
+using System;
 using Newtonsoft.Json;
 using System.IO;
+using System.Net.Http;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,87 +33,64 @@ namespace Sequence.Provider
             };
 
             var rpcRequestJson = JsonConvert.SerializeObject(request);
-            using var unityRequest = UnityWebRequest.Put(_url, rpcRequestJson);
-            unityRequest.SetRequestHeader("Content-Type", "application/json");
-            unityRequest.SetRequestHeader("Accept", "application/json");
-            unityRequest.method = UnityWebRequest.kHttpVerbPOST;
-            await unityRequest.SendWebRequest();
-
-            if (unityRequest.error != null)
-            {
-                Debug.Log("RequestError: " + unityRequest.error);
-                unityRequest.Dispose();
-                return default;
-            }
-            else
-            {
-                byte[] results = unityRequest.downloadHandler.data;
-                var responseJson = Encoding.UTF8.GetString(results);
-                RpcResponse result = JsonConvert.DeserializeObject<RpcResponse>(responseJson);
-                unityRequest.Dispose();
-                return result;
-            }
+            return await SendRequest(rpcRequestJson);
         }
 
         public async Task<RpcResponse> SendRequest(string method, object[] parameters)
         {
-            var request = new
-            {
-                jsonrpc = "2.0",
-                id = 1,
-                method = method,
-                @params = parameters
-                
-            };
-
-            var rpcRequestJson = JsonConvert.SerializeObject(request);
-            Debug.Log("request json :" + rpcRequestJson);
-            using var unityRequest = UnityWebRequest.Put(_url, rpcRequestJson);
-            
-            unityRequest.SetRequestHeader("Content-Type", "application/json");
-            unityRequest.SetRequestHeader("Accept", "application/json");
-            unityRequest.method = UnityWebRequest.kHttpVerbPOST;
-            await unityRequest.SendWebRequest();
-
-            if (unityRequest.error != null)
-            {
-                Debug.Log("RequestError: " + unityRequest.error);
-                unityRequest.Dispose();
-                return default;
-            }
-            else
-            {
-                byte[] results = unityRequest.downloadHandler.data;
-                var responseJson = Encoding.UTF8.GetString(results);
-                Debug.Log("response before parsing:" + responseJson);
-                RpcResponse result = JsonConvert.DeserializeObject<RpcResponse>(responseJson);
-                unityRequest.Dispose();
-                return result;
-            }
+            RpcRequest rpcRequest = new RpcRequest(BigInteger.One, method, parameters);
+            return await SendRequest(rpcRequest);
         }
 
         public async Task<RpcResponse> SendRequest(string requestJson)
         {
-            using var unityRequest = UnityWebRequest.Put(_url, requestJson);
+            using var request = UnityWebRequest.Put(_url, requestJson);
 
-            unityRequest.SetRequestHeader("Content-Type", "application/json");
-            unityRequest.SetRequestHeader("Accept", "application/json");
-            unityRequest.method = UnityWebRequest.kHttpVerbPOST;
-            await unityRequest.SendWebRequest();
+            request.SetRequestHeader("Content-Type", "application/json");
+            request.SetRequestHeader("Accept", "application/json");
+            request.SetRequestHeader("X-Access-Key", "YfeuczOMRyP7fpr1v7h8SvrCAAAAAAAAA"); // Todo: temporary access key while we wait for prod env deployment. Currently, we are using the staging env and we don't have a staging env for node gateway that we can hit publicly);
+            request.method = UnityWebRequest.kHttpVerbPOST;
+            
+            string curlRequest = $"curl -X {request.method} '{_url}' -H 'Content-Type: {request.GetRequestHeader("Content-Type")}' -H 'Accept: {request.GetRequestHeader("Accept")}' -H 'X-Access-Key: {request.GetRequestHeader("X-Access-Key")}' -d '{requestJson}'";
 
-            if (unityRequest.error != null)
+            try
             {
-                Debug.Log("RequestError: " + unityRequest.error);
-                unityRequest.Dispose();
-                return default;
+                await request.SendWebRequest();
+            
+                if (request.error != null || request.result != UnityWebRequest.Result.Success || request.responseCode < 200 || request.responseCode > 299)
+                {
+                    throw new Exception($"Error sending request to {_url}: {request.responseCode} {request.error}");
+                }
+                else
+                {
+                    byte[] results = request.downloadHandler.data;
+                    request.Dispose();
+                    var responseJson = Encoding.UTF8.GetString(results);
+                    try
+                    {
+                        RpcResponse result = JsonConvert.DeserializeObject<RpcResponse>(responseJson);
+                        return result;
+                    }
+                    catch (Exception e)
+                    {
+                        throw new Exception($"Error unmarshalling response from {_url}: {e.Message} | given: {responseJson}");
+                    }
+                }
             }
-            else
+            catch (HttpRequestException e)
             {
-                byte[] results = unityRequest.downloadHandler.data;
-                var responseJson = Encoding.UTF8.GetString(results);
-                RpcResponse result = JsonConvert.DeserializeObject<RpcResponse>(responseJson);
-                unityRequest.Dispose();
-                return result;
+                throw new Exception("HTTP Request failed: " + e.Message + " reason: " + Encoding.UTF8.GetString(request.downloadHandler.data)  + "\nCurl-equivalent request: " + curlRequest);
+            }
+            catch (FormatException e)
+            {
+                throw new Exception("Invalid URL format: " + e.Message + " reason: " + Encoding.UTF8.GetString(request.downloadHandler.data) + "\nCurl-equivalent request: " + curlRequest);
+            }
+            catch (FileLoadException e)
+            {
+                throw new Exception("File load exception: " + e.Message + " reason: " + Encoding.UTF8.GetString(request.downloadHandler.data) + "\nCurl-equivalent request: " + curlRequest);
+            }
+            catch (Exception e) {
+                throw new Exception("An unexpected error occurred: " + e.Message + " reason: " + Encoding.UTF8.GetString(request.downloadHandler.data) + "\nCurl-equivalent request: " + curlRequest);
             }
         }
     }
