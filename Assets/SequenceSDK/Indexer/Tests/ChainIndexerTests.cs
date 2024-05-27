3b20e18bb89f987e0bfc7a39398beb7bbbb2959a
BellringerStudios <quinn.purdy.gamedev@gmail.com>
2024-05-27 11:45:15 -0400

    Setup test assembly for Indexer. Added basic test to make sure we can create an indexer for each chain

---
+++ Assets/SequenceSDK/Indexer/Tests/ChainIndexerTests.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using NUnit.Framework;
+using Sequence;
+using Sequence.Utils;
+
+namespace Sequence.Indexer.Tests
+{
+    public class ChainIndexerTests
+    {
+        private static List<Chain> chainIdCases = EnumExtensions.GetEnumValuesAsList<Chain>();
+    
+        [TestCaseSource(nameof(chainIdCases))]
+        public void CreateChainIndexerForChain(Chain chain)
+        {
+            if (chain == Chain.None) return;
+            try
+            {
+                ChainIndexer chainIndexer = new ChainIndexer(chain);
+                Assert.AreEqual(BigInteger.Parse(chain.GetChainId()), chainIndexer.GetChainID());
+                Assert.AreEqual(chain, chainIndexer.GetChain());
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Encountered exception when none was expected: " + e.Message);
+            }
+        }
+    }
+}
