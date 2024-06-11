e243a0f4b6155d5d51cf06c5ff6a2a83577f00d0
Quinn Purdy <qp@horizon.io>
2024-06-11 11:36:22 -0400

    Decorate storage key with the Application name and company name to help ensure uniqueness

--- Assets/Sequence/SequenceSDK/Utils/iOSKeychainStorage.cs
+++ Assets/Sequence/SequenceSDK/Utils/iOSKeychainStorage.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.InteropServices;
+using UnityEngine;
 
 namespace Sequence.Utils
 {
@@ -22,15 +23,15 @@ namespace Sequence.Utils
         private static void SaveKeychainValue(string key, string value) { }
         private static IntPtr GetKeychainValue(string key) { return IntPtr.Zero; }
 #endif
-
+        
         public void StoreString(string key, string value)
         {
-            SaveKeychainValue(key, value);
+            SaveKeychainValue(DecorateKey(key), value);
         }
 
         public string RetrieveString(string key)
         {
-            IntPtr valuePtr = GetKeychainValue(key);
+            IntPtr valuePtr = GetKeychainValue(DecorateKey(key));
             if (valuePtr != IntPtr.Zero)
             {
                 string value = Marshal.PtrToStringUTF8(valuePtr);
@@ -38,6 +39,11 @@ namespace Sequence.Utils
                 return value;
             }
             return null;
+        }
+
+        private string DecorateKey(string key)
+        {
+            return Application.companyName + "-" + Application.productName + "-" + key; // This should help ensure the key is unique for each app
         }
     }
 }
