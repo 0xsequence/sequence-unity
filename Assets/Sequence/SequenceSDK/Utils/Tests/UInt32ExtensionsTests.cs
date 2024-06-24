90adae59fe8d56eb70fa4479dcc8192b2434a2d2
Quinn Purdy <49766546+BellringerQuinn@users.noreply.github.com>
2024-06-24 17:23:47 -0400

    Move tests under a namespace and to proper location (#138)

---
+++ Assets/Sequence/SequenceSDK/Utils/Tests/UInt32ExtensionsTests.cs
@@ -0,0 +1,20 @@
+using System;
+using NUnit.Framework;
+using Sequence.Utils;
+
+namespace Sequence.Utils.Tests
+{
+    public class UInt32ExtensionsTests
+    {
+        [TestCase((UInt32)0x00000000, new byte[] { 0x00, 0x00, 0x00, 0x00 })]
+        [TestCase((UInt32)0x12345678, new byte[] { 0x12, 0x34, 0x56, 0x78 })]
+        [TestCase((UInt32)0xAABBCCDD, new byte[] { 0xAA, 0xBB, 0xCC, 0xDD })]
+        [TestCase((UInt32)0xFFFFFFFF, new byte[] { 0xFF, 0xFF, 0xFF, 0xFF })]
+        public void TestToByteArray(UInt32 value, byte[] expected)
+        {
+            byte[] byteArray = value.ToByteArray();
+
+            Assert.AreEqual(expected, byteArray);
+        }
+    }
+}
