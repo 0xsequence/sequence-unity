102e85872c3c67c80f14ed74a80fdbe2b82352fb
Quinn Purdy <qp@horizon.io>
2024-10-25 14:32:06 -0400

    Fixed the DelayedEncodeData json serialization such that it alphabetizes the fields in complex objects, including arrays and nested objects

--- Packages/Sequence-Unity/Sequence/SequenceSDK/EmbeddedWallet/DataTypes/ParameterTypes/DelayedEncodeData.cs
+++ Packages/Sequence-Unity/Sequence/SequenceSDK/EmbeddedWallet/DataTypes/ParameterTypes/DelayedEncodeData.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -69,20 +71,36 @@ namespace Sequence.EmbeddedWallet
 
         private JToken SerializeAlphabetically(object arg, JsonSerializer serializer)
         {
-            try
-            {
-                JObject jsonArg = JObject.FromObject(arg, serializer);
-                
-                JObject sortedJsonArg = new JObject(
-                    jsonArg.Properties().OrderBy(p => p.Name)
-                );
+            // Convert the input object to a JToken.
+            JToken token = JToken.FromObject(arg, serializer);
 
-                return sortedJsonArg;
-            }
-            catch (Exception)
+            // Handle arrays recursively.
+            if (token.Type == JTokenType.Array)
             {
-                return JToken.FromObject(arg, serializer);
+                JArray array = new JArray();
+                foreach (var element in token.Children())
+                {
+                    array.Add(SerializeAlphabetically(element, serializer)); // Recursively process each element.
+                }
+                return array;
             }
+
+            // Handle objects recursively and sort properties by name.
+            if (token.Type == JTokenType.Object)
+            {
+                JObject jsonObject = (JObject)token;
+                JObject sortedObject = new JObject(
+                    jsonObject.Properties()
+                        .OrderBy(p => p.Name)
+                        .Select(p => new JProperty(
+                            p.Name, SerializeAlphabetically(p.Value, serializer))) // Recursively process each property value.
+                );
+                return sortedObject;
+            }
+
+            // Return the token as-is for primitive types.
+            return token;
         }
+
     }
 }
