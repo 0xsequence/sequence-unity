2b8d94808673782c6503593c3e7b8aa143d74b8a
Quinn Purdy <qp@horizon.io>
2024-11-12 11:12:28 -0500

    Fix broken generate listing transaction test implementation

--- Assets/SequenceSDK/Marketplace/MarketplaceCheckoutTests.cs
+++ Assets/SequenceSDK/Marketplace/MarketplaceCheckoutTests.cs
@@ -14,7 +14,7 @@ namespace Sequence.Marketplace
         private IWallet _testWallet =
             new SequenceWallet(new Address("0xD2eFbb2f18bfE3D265b26D2ACe83400A65335a07"), "", null);
         
-        [TestCase(0)]
+        [TestCase(1)]
         [TestCase(3)]
         public async Task TestGetCheckoutOptions(int amount)
         {
@@ -39,7 +39,7 @@ namespace Sequence.Marketplace
             Assert.AreNotEqual(TransactionCrypto.unknown, options.crypto);
         }
 
-        [TestCase(0)]
+        [TestCase(1)]
         [TestCase(3)]
         public async Task TestGenerateBuyTransaction(int amount)
         {
@@ -66,7 +66,7 @@ namespace Sequence.Marketplace
             }
         }
 
-        [TestCase(0)]
+        [TestCase(1)]
         [TestCase(3)]
         public async Task TestGenerateSellTransaction(int amount)
         {
@@ -93,36 +93,32 @@ namespace Sequence.Marketplace
             }
         }
 
-        [TestCase(0)]
-        // [TestCase(3)] todo add back in once we have test data that returns more than 1 offer
+        [TestCase(1)]
+        [TestCase(2)]
         public async Task TestGenerateListingTransaction(int amount)
         {
-            _collectibleOrders = await OrderFetcher.FetchOffers();
-            List<Order> orders = new List<Order>();
-            for (int i = 0; i < _collectibleOrders.Length; i++)
-            {
-                if (_collectibleOrders[i].order.status == OrderStatus.active)
-                {
-                    orders.Add(_collectibleOrders[i].order);
-                    if (orders.Count == amount)
-                    {
-                        break;
-                    }
-                }
-            }
-            Checkout checkout = new Checkout(_testWallet, Chain.TestnetPolygonAmoy);
+            Checkout checkout = new Checkout(_testWallet, Chain.ArbitrumNova);
+            ChainIndexer indexer = new ChainIndexer(Chain.ArbitrumNova);
+            Address USDC = new Address("0x750ba8b76187092B0D1E87E28daaf484d1b5273b");
+            
+            GetTokenBalancesReturn balancesReturn = await indexer.GetTokenBalances(
+                new GetTokenBalancesArgs(_testWallet.GetWalletAddress(), "0x0ee3af1874789245467e7482f042ced9c5171073"));
+            Assert.IsNotNull(balancesReturn);
+            TokenBalance[] balances = balancesReturn.balances;
+            Assert.IsNotNull(balances);
+            Assert.GreaterOrEqual(balances.Length, amount);
 
             for (int i = 0; i < amount; i++)
             {
-                Step[] steps = await checkout.GenerateListingTransaction(new Address(orders[i].collectionContractAddress), orders[i].tokenId, 
-                    BigInteger.Parse(orders[i].quantityAvailable), ContractType.ERC1155, new Address(orders[i].priceCurrencyAddress), 
+                Step[] steps = await checkout.GenerateListingTransaction(new Address(balances[i].contractAddress), balances[i].tokenID.ToString(), 
+                    balances[i].balance, balances[i].contractType.AsMarketplaceContractType(), USDC, 
                     1, DateTime.Now + TimeSpan.FromMinutes(30));
                 Assert.IsNotNull(steps);
                 Assert.Greater(steps.Length, 0);
             }
         }
 
-        [TestCase(0)]
+        [TestCase(1)]
         [TestCase(3)]
         public async Task TestGenerateOfferTransaction(int amount)
         {
--- Packages/Sequence-Unity/Sequence/SequenceSDK/Marketplace/DataTypes/ContractType.cs
+++ Packages/Sequence-Unity/Sequence/SequenceSDK/Marketplace/DataTypes/ContractType.cs
@@ -13,4 +13,22 @@ namespace Sequence.Marketplace
         ERC721,
         ERC1155
     }
+
+    public static class ContractTypeExtensions
+    {
+        public static ContractType AsMarketplaceContractType(this Sequence.ContractType contractType)
+        {
+            switch (contractType)
+            {
+                case Sequence.ContractType.ERC20:
+                    return ContractType.ERC20;
+                case Sequence.ContractType.ERC721:
+                    return ContractType.ERC721;
+                case Sequence.ContractType.ERC1155:
+                    return ContractType.ERC1155;
+                default:
+                    return ContractType.UNKNOWN;
+            }
+        }
+    }
 }
