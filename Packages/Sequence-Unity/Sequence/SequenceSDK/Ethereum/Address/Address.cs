71bb8e0b1604ffa9da04974c749e3beebc151cc5
Quinn Purdy <qp@horizon.io>
2024-12-12 14:11:10 -0500

    Integrated the swap api

--- Packages/Sequence-Unity/Sequence/SequenceSDK/Ethereum/Address/Address.cs
+++ Packages/Sequence-Unity/Sequence/SequenceSDK/Ethereum/Address/Address.cs
@@ -1,7 +1,10 @@
 using System;
+using Newtonsoft.Json;
 using Sequence.Utils;
 
 namespace Sequence {
+    
+    [JsonConverter(typeof(AddressJsonConverter))]
     public class Address {
         public readonly string Value;
 
@@ -38,6 +41,45 @@ namespace Sequence {
 
             Address address = (Address)obj;
             return this.Value == address.Value;
+        }
+    }
+
+    public class AddressJsonConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is Address address)
+            {
+                writer.WriteValue(address.Value);
+            }
+            else
+            {
+                throw new JsonSerializationException("Expected Address object.");
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+            {
+                string addressString = (string)reader.Value;
+            
+                try
+                {
+                    return new Address(addressString);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    throw new JsonSerializationException("Invalid address format.");
+                }
+            }
+
+            throw new JsonSerializationException("Expected a string value.");
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
         }
     }
 }
