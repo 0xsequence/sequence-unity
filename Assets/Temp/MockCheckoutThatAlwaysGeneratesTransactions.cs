f1ead461aea8391e2506498932a2a408f493ae18
Quinn Purdy <qp@horizon.io>
2025-01-17 13:50:33 -0500

    Revert "Cleanup - remove code I'm not as confident in and remove Temp scene + scripts"
    This reverts commit 2393d35da87ed25416220df64b8f873e9546efa8.

---
+++ Assets/Temp/MockCheckoutThatAlwaysGeneratesTransactions.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using Sequence;
+using Sequence.Marketplace;
+using CheckoutOptions = Sequence.Marketplace.CheckoutOptions;
+using ContractType = Sequence.Marketplace.ContractType;
+
+namespace Temp
+{
+    public class MockCheckoutThatAlwaysGeneratesTransactions : ICheckout
+    {
+        public event Action<Sequence.Marketplace.CheckoutOptions> OnCheckoutOptionsReturn;
+        public event Action<string> OnCheckoutOptionsError;
+        public Task<Sequence.Marketplace.CheckoutOptions> GetCheckoutOptions(CheckoutOptionsMarketplaceOrder[] orders, int additionalFeeBps = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Sequence.Marketplace.CheckoutOptions> GetCheckoutOptions(Order[] orders, int additionalFeeBps = 0)
+        {
+            return new Sequence.Marketplace.CheckoutOptions(TransactionCrypto.all, Array.Empty<TransactionSwapProvider>(), Array.Empty<TransactionNFTCheckoutProvider>(), new TransactionOnRampProvider[]
+                {
+                    TransactionOnRampProvider.transak
+                });
+        }
+
+        public event Action<Step[]> OnTransactionStepsReturn;
+        public event Action<string> OnTransactionStepsError;
+        public async Task<Step[]> GenerateBuyTransaction(Order order, BigInteger amount, AdditionalFee additionalFee = null)
+        {
+            return new Step[]
+            {
+                new Step(StepType.unknown, "data", "to", "value", null, null)
+            };
+        }
+
+        public async Task<Step[]> GenerateSellTransaction(Order order, BigInteger amount, AdditionalFee additionalFee = null)
+        {
+            return new Step[]
+            {
+                new Step(StepType.unknown, "data", "to", "value", null, null)
+            };
+        }
+
+        public async Task<Step[]> GenerateListingTransaction(Address collection, string tokenId, BigInteger amount, ContractType contractType,
+            Address currencyTokenAddress, BigInteger pricePerToken, DateTime expiry,
+            OrderbookKind orderbookKind = OrderbookKind.sequence_marketplace_v2)
+        {
+            return new Step[]
+            {
+                new Step(StepType.unknown, "data", "to", "value", null, null)
+            };
+        }
+
+        public async Task<Step[]> GenerateOfferTransaction(Address collection, string tokenId, BigInteger amount, ContractType contractType,
+            Address currencyTokenAddress, BigInteger pricePerToken, DateTime expiry,
+            OrderbookKind orderbookKind = OrderbookKind.sequence_marketplace_v2)
+        {
+            
+            return new Step[]
+            {
+                new Step(StepType.unknown, "data", "to", "value", null, null)
+            };
+        }
+
+        public async Task<Step[]> GenerateCancelTransaction(Address collection, string orderId,
+            MarketplaceKind marketplaceKind = MarketplaceKind.sequence_marketplace_v2)
+        {
+            
+            return new Step[]
+            {
+                new Step(StepType.unknown, "data", "to", "value", null, null)
+            };
+        }
+    }
+}
