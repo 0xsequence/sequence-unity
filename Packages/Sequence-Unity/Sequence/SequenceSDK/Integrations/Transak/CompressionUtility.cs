1de36e64e30ac2a1ee44931bab8e33cb5573ad8a
Quinn Purdy <qp@horizon.io>
2025-01-31 12:18:30 -0500

    Fix Transak calldata compression utility

--- Packages/Sequence-Unity/Sequence/SequenceSDK/Integrations/Transak/CompressionUtility.cs
+++ Packages/Sequence-Unity/Sequence/SequenceSDK/Integrations/Transak/CompressionUtility.cs
@@ -1,29 +1,33 @@
 using System;
 using System.IO;
-using System.IO.Compression;
 using System.Text;
+using Ionic.Zlib;
 
 namespace Sequence.Integrations.Transak
 {
     public static class CompressionUtility
     {
-        public static byte[] Deflate(string data)
+        private static byte[] Deflate(string data)
         {
             byte[] inputBytes = Encoding.UTF8.GetBytes(data);
-
             using var outputStream = new MemoryStream();
-            using (var deflateStream = new DeflateStream(outputStream, CompressionLevel.Optimal))
+            using (var deflater = new ZlibStream(outputStream, CompressionMode.Compress, CompressionLevel.Default))
             {
-                deflateStream.Write(inputBytes, 0, inputBytes.Length);
+                deflater.Write(inputBytes, 0, inputBytes.Length);
             }
-
             return outputStream.ToArray();
         }
 
         public static string DeflateAndEncodeToBase64(string data)
         {
             byte[] compressedData = Deflate(data);
-            return Convert.ToBase64String(compressedData);
+            return Base64UrlEncode(compressedData);
+        }
+        
+        private static string Base64UrlEncode(byte[] input)
+        {
+            string base64 = Convert.ToBase64String(input);
+            return base64;
         }
     }
 }
