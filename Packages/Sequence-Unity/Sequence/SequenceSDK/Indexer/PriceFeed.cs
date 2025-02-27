dbef4d7693f2ea18a9407dcaa8f317cf5d466123
Quinn Purdy <qp@horizon.io>
2025-02-27 08:44:32 -0500

    Added price feeds

---
+++ Packages/Sequence-Unity/Sequence/SequenceSDK/Indexer/PriceFeed.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Numerics;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Sequence.Config;
+using Sequence.Utils;
+using UnityEngine.Scripting;
+
+namespace Sequence
+{
+    public class PriceFeed
+    {
+        [Serializable]
+        [JsonConverter(typeof(TokenConverter))]
+        public class Token
+        {
+            public Chain Chain;
+            public Address Contract;
+            public string TokenId;
+
+            public Token(Chain chain, Address contract, string tokenId = "")
+            {
+                Chain = chain;
+                Contract = contract;
+                TokenId = tokenId;
+            }
+
+            [Preserve]
+            [JsonConstructor]
+            public Token(BigInteger chainId, string contractAddress, string tokenId = "")
+            {
+                Chain = ChainDictionaries.ChainById[chainId.ToString()];
+                Contract = new Address(contractAddress);
+                TokenId = tokenId;
+            }
+        }
+
+        private class TokenConverter : JsonConverter<Token>
+        {
+            public override void WriteJson(JsonWriter writer, Token value, JsonSerializer serializer)
+            {
+                var jsonObject = new JObject
+                {
+                    ["chainId"] = ulong.Parse(ChainDictionaries.ChainIdOf[value.Chain]),
+                    ["contractAddress"] = value.Contract.ToString(),
+                    ["tokenId"] = value.TokenId
+                };
+                jsonObject.WriteTo(writer);
+            }
+
+            public override Token ReadJson(JsonReader reader, Type objectType, Token existingValue, bool hasExistingValue, JsonSerializer serializer)
+            {
+                var jsonObject = JObject.Load(reader);
+
+                BigInteger chainId = jsonObject["chainId"]?.Value<ulong>() ?? 0;
+                string contractAddress = jsonObject["contractAddress"]?.Value<string>();
+                string tokenId = jsonObject["tokenId"]?.Value<string>();
+
+                return new Token(chainId, contractAddress, tokenId);
+            }
+        }
+        
+        private const string _devUrl = "https://dev-api.sequence.app/rpc/API";
+        private const string _prodUrl = "https://api.sequence.app/rpc/API";
+        
+        private IHttpHandler _httpHandler;
+        private string _baseUrl;
+        
+        public PriceFeed(IHttpHandler httpHandler = null)
+        {
+            _httpHandler = httpHandler;
+            if (_httpHandler == null)
+            {
+                _httpHandler = new HttpHandler(SequenceConfig.GetConfig(SequenceService.Stack).BuilderAPIKey);
+            }
+            
+#if SEQUENCE_DEV_STACK || SEQUENCE_DEV
+            _baseUrl = _devUrl;
+#else
+            _baseUrl = _prodUrl;
+#endif
+        }
+
+        public async Task<TokenPrice[]> GetCoinPrices(params Token[] tokens)
+        {
+            GetTokenPricesArgs args = new GetTokenPricesArgs(tokens);
+            GetTokenPricesReturn response = await _httpHandler.HttpPost<GetTokenPricesReturn>(_baseUrl.AppendTrailingSlashIfNeeded() + "GetCoinPrices", args);
+            return response.tokenPrices;
+        }
+
+        public async Task<TokenPrice[]> GetCollectiblePrices(params Token[] tokens)
+        {
+            GetTokenPricesArgs args = new GetTokenPricesArgs(tokens);
+            GetTokenPricesReturn response = await _httpHandler.HttpPost<GetTokenPricesReturn>(_baseUrl.AppendTrailingSlashIfNeeded() + "GetCollectiblePrices", args);
+            return response.tokenPrices;
+        }
+    }
+}
