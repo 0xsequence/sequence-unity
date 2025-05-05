7eb0b6803c9cacd3b3a3253eb95c447c75a155e7
Quinn Purdy <qp@horizon.io>
2025-05-05 11:36:41 -0400

    Move Android secure storage plugin into samples folder so that it can be imported into the assets folder - previously, we were encountering build issues where when secure storage wasn't enabled, we weren't able to toggle this plugin off as it was in the packages folder and immutable; this should fix this issue

--- Packages/Sequence-Unity/Editor/AndroidDependencyManager.cs
+++ Packages/Sequence-Unity/Editor/AndroidDependencyManager.cs
@@ -1,9 +1,9 @@
-using Sequence.Config;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
-using UnityEditor.Callbacks;
 using UnityEngine;
+using System.Linq;
+using Sequence.Config;
 
 namespace Sequence.Editor
 {
@@ -14,26 +14,39 @@ namespace Sequence.Editor
     /// </summary>
     public class AndroidDependencyManager : IPreprocessBuildWithReport
     {
-        public const string SecureStoragePluginPath = "Packages/xyz.0xsequence.waas-unity/Plugins/Android/AndroidKeyBridge.java";
-        
+        private const string SecureStoragePluginFilename = "AndroidKeyBridge.java";
+
         public int callbackOrder => 0;
-        
+
         public void OnPreprocessBuild(BuildReport report)
         {
 #if UNITY_ANDROID
             BuildTarget target = report.summary.platform;
             SequenceConfig config = SequenceConfig.GetConfig();
-            
-            PluginImporter pluginImporter = AssetImporter.GetAtPath(SecureStoragePluginPath) as PluginImporter;
 
-            if (pluginImporter == null)
+            string[] matchingAssets = AssetDatabase.FindAssets(SecureStoragePluginFilename);
+
+            string pluginPath = matchingAssets
+                .Select(guid => AssetDatabase.GUIDToAssetPath(guid))
+                .FirstOrDefault(path => path.EndsWith(SecureStoragePluginFilename));
+
+            if (string.IsNullOrEmpty(pluginPath))
             {
-                Debug.LogWarning($"Plugin not found at path: {SecureStoragePluginPath}");
+                Debug.LogWarning($"Secure Storage plugin '{SecureStoragePluginFilename}' not found in project.");
                 return;
             }
-            
+
+            PluginImporter pluginImporter = AssetImporter.GetAtPath(pluginPath) as PluginImporter;
+            if (pluginImporter == null)
+            {
+                Debug.LogWarning($"Unable to create {nameof(PluginImporter)} instance at path: {pluginPath}");
+                return;
+            }
+
             pluginImporter.SetCompatibleWithPlatform(target, config.StoreSessionPrivateKeyInSecureStorage);
             pluginImporter.SaveAndReimport();
+            Debug.Log(
+                $"Secure Storage plugin compatibility set to {config.StoreSessionPrivateKeyInSecureStorage} for path: {pluginPath}");
 #endif
         }
     }
