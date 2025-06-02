d69ee913c7ad38f0137a4b3eb5f0316b1b300424
Andy Grüning <andygruening@gmail.com>
2025-06-02 14:01:20 +0200

    added permission primitives and its decoding/encoding functions

---
+++ Packages/Sequence-Unity/Sequence/SequenceSDK/EcosystemWallet/Primitives/Permission/Permission.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using Sequence.Utils;
+
+namespace Sequence.EcosystemWallet.Primitives
+{
+    [Serializable]
+    public class Permission
+    {
+        public const int MAX_PERMISSIONS_COUNT = (1 << 7) - 1;
+        public const int MAX_RULES_COUNT = (1 << 8) - 1;
+        
+        public Address target;
+        public ParameterRule[] rules;
+
+        public byte[] Encode()
+        {
+            if (rules.Length > MAX_RULES_COUNT) {
+                throw new Exception("Too many rules");
+            }
+
+            List<byte> result = new();
+            result.AddRange(target.ToString().HexStringToByteArray().PadLeft(20));
+            result.Add((byte)rules.Length);
+
+            foreach (var rule in rules) {
+                result.AddRange(rule.Encode());
+            }
+
+            return result.ToArray();
+        }
+
+        public static (Permission Permission, int Consumed) Decode(byte[] data, int offset)
+        {
+            if (data.Length < offset + 21)
+                throw new Exception("Data too short for permission");
+
+            var target = new Address(data.AsSpan(offset, 20).ToArray());
+            offset += 20;
+
+            int rulesLength = data[offset];
+            offset += 1;
+
+            var rules = new ParameterRule[rulesLength];
+            const int ruleSize = 97;
+            
+            for (var i = 0; i < rulesLength; i++) {
+                if (data.Length < offset + ruleSize)
+                    throw new Exception("Data too short for parameter rule");
+
+                var ruleBytes = data.AsSpan(offset, ruleSize).ToArray();
+                var rule = ParameterRule.Decode(ruleBytes);
+                rules[i] = rule;
+                offset += ruleSize;
+            }
+
+            return (new Permission {
+                target = target,
+                rules = rules
+            }, 20 + 1 + ruleSize * rulesLength);
+        }
+    }
+}
