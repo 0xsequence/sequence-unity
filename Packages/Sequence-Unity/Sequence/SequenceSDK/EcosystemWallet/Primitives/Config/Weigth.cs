4ae07900997d6ae5f93ece25036386045e13c183
Quinn Purdy <qp@horizon.io>
2025-06-12 15:58:09 -0400

    Added ability to get weight of a topology or config - adding Weight class

---
+++ Packages/Sequence-Unity/Sequence/SequenceSDK/EcosystemWallet/Primitives/Config/Weigth.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Numerics;
+
+namespace Sequence.EcosystemWallet.Primitives
+{
+    internal class Weigth
+    {
+        public BigInteger weight;
+        public BigInteger maxWeight;
+        
+        public static Weigth GetWeight(RawTopology topology, Func<Leaf, bool> canSign)
+        {
+            if (topology == null)
+            {
+                return new Weigth { weight = 0, maxWeight = 0 };
+            }
+
+            if (topology.IsNode())
+            {
+                var left = GetWeight(topology.Node.left, canSign);
+                var right = GetWeight(topology.Node.right, canSign);
+                return new Weigth 
+                { 
+                    weight = left.weight + right.weight, 
+                    maxWeight = left.maxWeight + right.maxWeight 
+                };
+            }
+            else if (topology.IsLeaf())
+            {
+                return GetWeightForLeaf(topology.Leaf, canSign);
+            }
+
+            return new Weigth { weight = 0, maxWeight = 0 };
+        }
+
+        public static Weigth GetWeight(RawConfig configuration, Func<Leaf, bool> canSign)
+        {
+            if (configuration?.topology == null)
+            {
+                return new Weigth { weight = 0, maxWeight = 0 };
+            }
+
+            return GetWeight(configuration.topology, canSign);
+        }
+
+        private static Weigth GetWeightForLeaf(RawLeaf leaf, Func<Leaf, bool> canSign)
+        {
+            if (leaf is SignedSignerLeaf signedSigner)
+            {
+                return new Weigth { weight = signedSigner.weight, maxWeight = signedSigner.weight };
+            }
+            if (leaf is SignedSapientSignerLeaf signedSapient)
+            {
+                return new Weigth { weight = signedSapient.weight, maxWeight = signedSapient.weight };
+            }
+            if (leaf is RawSignerLeaf rawSigner)
+            {
+                return new Weigth { weight = rawSigner.weight, maxWeight = rawSigner.weight };
+            }
+            if (leaf is SignerLeaf signer)
+            {
+                BigInteger maxWeight = canSign(signer) ? signer.weight : 0;
+                return new Weigth { weight = 0, maxWeight = maxWeight };
+            }
+            if (leaf is SapientSignerLeaf sapient)
+            {
+                BigInteger maxWeight = canSign(sapient) ? sapient.weight : 0;
+                return new Weigth { weight = 0, maxWeight = maxWeight };
+            }
+            if (leaf is SubdigestLeaf || leaf is AnyAddressSubdigestLeaf || leaf is NodeLeaf)
+            {
+                return new Weigth { weight = 0, maxWeight = 0 };
+            }
+            if (leaf is RawNestedLeaf rawNested)
+            {
+                var result = GetWeight(rawNested.tree, canSign);
+                BigInteger weight = result.weight >= rawNested.threshold ? rawNested.weight : 0;
+                BigInteger maxWeight = result.maxWeight >= rawNested.threshold ? rawNested.weight : 0;
+                return new Weigth { weight = weight, maxWeight = maxWeight };
+            }
+
+            return new Weigth { weight = 0, maxWeight = 0 };
+        }
+    }
+}
