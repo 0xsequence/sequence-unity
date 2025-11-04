c2005aa314139a500b56c3f7d89ba26a4e6ddc4c
Andy Grüning <andygruening@gmail.com>
2025-11-04 12:14:11 +0100

    added DidReloadScripts inside VersionSetter

--- Packages/Sequence-Unity/Sequence/SequenceSDK/Config/Editor/VersionSetter.cs
+++ Packages/Sequence-Unity/Sequence/SequenceSDK/Config/Editor/VersionSetter.cs
@@ -1,25 +1,19 @@
 using UnityEditor;
 using System.IO;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace Sequence.Config.Editor
 {
-    [InitializeOnLoad]
     public static class VersionSetter
     {
-        static VersionSetter()
-        {
-            UpdateVersionFileOnImport();
-        }
-
-        private static void UpdateVersionFileOnImport()
-        {
-            EditorApplication.delayCall += InjectSDKVersionIntoResources;
-        }
-
         // Because of the InitializeOnLoad attribute, this method will be called whenever code is recompiled in the editor
+        
+        [DidReloadScripts]
         private static void InjectSDKVersionIntoResources()
         {
+            Debug.Log("Injecting SDK version into Resources");
+            
             string version = PackageVersionReader.GetVersion();
             string versionFilePath = "Assets/Resources/sequence-unity-version.txt";
 
