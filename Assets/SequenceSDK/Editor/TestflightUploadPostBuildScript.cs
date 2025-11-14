058ed3511d966ac406c244d72e8586ba40f74240
Andy Grüning <andygruening@gmail.com>
2025-11-14 10:08:03 +0100

    added TestflightUploadPostBuildScript

---
+++ Assets/SequenceSDK/Editor/TestflightUploadPostBuildScript.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using System.Diagnostics;
+using System.IO;
+using Debug = UnityEngine.Debug;
+
+namespace Sequence.Editor
+{
+    public class TestflightUploadPostBuildScript : IPostprocessBuildWithReport
+    {
+        public int callbackOrder => 999;
+
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            if (report.summary.platform != BuildTarget.iOS ||
+                Environment.GetEnvironmentVariable("ENABLE_TESTFLIGHT_UPLOAD") != "TRUE")
+                return;
+            
+            Debug.Log("Uploading iOS binary to TestFlight...");
+
+            var exportPath = report.summary.outputPath;
+            var ipaPath = Path.Combine(exportPath, "build.ipa");
+
+            var keyId = Environment.GetEnvironmentVariable("APPSTORE_CONNECT_KEY_ID");
+            var issuerId = Environment.GetEnvironmentVariable("APPSTORE_CONNECT_ISSUER_ID");
+            var privateKey = Environment.GetEnvironmentVariable("APPSTORE_CONNECT_P8");
+            
+            var keyDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".appstoreconnect/private_keys");
+            
+            Directory.CreateDirectory(keyDir);
+            var keyPath = Path.Combine(keyDir, $"AuthKey_{keyId}.p8");
+            File.WriteAllText(keyPath, privateKey);
+
+            var cmd = $"xcrun altool --upload-app -f \"{ipaPath}\" -t ios --apiKey {keyId} --apiIssuer {issuerId}";
+            RunCommand("/bin/bash", $"-c \"{cmd}\"");
+        }
+
+        static void RunCommand(string cmd, string args)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = cmd,
+                    Arguments = args,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            Debug.Log(process.StandardOutput.ReadToEnd());
+            Debug.LogError(process.StandardError.ReadToEnd());
+            process.WaitForExit();
+        }
+    }
+}
