e54c8fbb3b2e954eebf110d490f3b53d1e75c350
Andy Grüning <andygruening@gmail.com>
2025-11-26 22:09:49 +0100

    memorize guard configs

--- Packages/Sequence-Unity/Sequence/SequenceSDK/EcosystemWallet/Guard/GuardStorage.cs
+++ Packages/Sequence-Unity/Sequence/SequenceSDK/EcosystemWallet/Guard/GuardStorage.cs
@@ -1,25 +1,33 @@
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using Sequence.Utils.SecureStorage;
-using UnityEngine;
 
 namespace Sequence.EcosystemWallet
 {
     public class GuardStorage
     {
+        private static readonly Dictionary<Address, GuardConfig> Configs = new();
+        
         private readonly ISecureStorage _storage = SecureStorageFactory.CreateSecureStorage();
 
         public GuardConfig GetConfig(Address address)
         {
+            if (Configs.TryGetValue(address, out var memorizedConfig))
+                return memorizedConfig;
+            
             var json = _storage.RetrieveString(BuildStorageKey(address));
-            Debug.Log($"{json}");
-            return string.IsNullOrEmpty(json) ? null : 
-                JsonConvert.DeserializeObject<GuardConfig>(json);
+            var config = string.IsNullOrEmpty(json) ? null : JsonConvert.DeserializeObject<GuardConfig>(json);
+            if (config != null)
+                Configs[address] = config;
+            
+            return config;
         }
         
         public void SaveConfig(Address address, GuardConfig config)
         {
             var json = JsonConvert.SerializeObject(config);
             _storage.StoreString(BuildStorageKey(address), json);
+            Configs[address] = config;
         }
 
         private string BuildStorageKey(Address address)
